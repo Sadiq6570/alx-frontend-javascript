094e770a90169565ed84d5d90eab3a0fdd6e00ff
Sadiq6570 <sadeeqmuhtar67@gmail.com>
2024-07-31 04:53:50 +0000

    ES6

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,17 +1,20 @@
+export default function updateStudentGradeByCity(studentList, city, newGrades) {
+  // filter to get students in the specified city
+  const studentByCity = studentList.filter((student) => student.location === city);
 
-export default function updateStudentGradeByCity(listStudents, city, newGrades) {
-  const students = listStudents.filter((student) => student.location === city);
-  for (const student of students) {
-    student.grade = 'N/A';
-  }
-  const newStudents = students.map((student) => {
-    const single = student;
-    for (const grade of newGrades) {
-      if (single.id === grade.studentId) {
-        single.grade = grade.grade;
-      }
-    }
-    return single;
+  // use map to update grades
+  const updatedStudents = studentByCity.map((student) => {
+    // find the grade object for the student in newGrades array
+    const gradeObject = newGrades.find((grade) => grade.studentId === student.id);
+
+    // If grade is found, update else set N/A
+    const updatedGrade = gradeObject ? gradeObject.grade : 'N/A';
+
+    // Return a new obj with updated grade
+    return {
+      ...student,
+      grade: updatedGrade,
+    };
   });
-  return newStudents;
+  return updatedStudents;
 }
--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,3 +1,4 @@
 export default function hasValuesFromArray(set, array) {
-  return array.every((value) => set.has(value));
+  const hasElements = array.every((value) => set.has(value));
+  return hasElements;
 }
